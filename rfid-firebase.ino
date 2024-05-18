d1ed23549de7c62b35998722007c8ee9236a2da3
Khalidoukh <76837293+Khalidoukh@users.noreply.github.com>
2024-05-18 12:23:50 +0100

    Add files via upload

---
+++ rfid-firebase.ino
@@ -0,0 +1,69 @@
+/*
+ * This ESP8266 NodeMCU code was developed by newbiely.com
+ *
+ * This ESP8266 NodeMCU code is made available for public use without any restriction
+ *
+ * For comprehensive instructions and wiring diagrams, please visit:
+ * https://newbiely.com/tutorials/esp8266/esp8266-rfid-servo-motor
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+
+#define SS_PIN  D8 // The ESP8266 pin D8
+#define RST_PIN D2 // The ESP8266 pin D2
+#define SERVO_PIN D1  // The ESP8266 pin connects to servo motor
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+Servo servo;
+
+byte authorizedUID[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+int angle = 0; // The current angle of servo motor
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+  servo.attach(SERVO_PIN);
+  servo.write(angle); // rotate servo motor to 0°
+
+  Serial.println("Tap RFID/NFC Tag on reader");
+}
+
+void loop() {
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+
+      if (rfid.uid.uidByte[0] == authorizedUID[0] &&
+          rfid.uid.uidByte[1] == authorizedUID[1] &&
+          rfid.uid.uidByte[2] == authorizedUID[2] &&
+          rfid.uid.uidByte[3] == authorizedUID[3] ) {
+        Serial.println("Authorized Tag");
+
+        // change angle of servo motor
+        if (angle == 0)
+          angle = 90;
+        else //if(angle == 90)
+          angle = 0;
+
+        // rotate the servo motor to the angle position
+        servo.write(angle);
+        Serial.print("Rotate Servo Motor to ");
+        Serial.print(angle);
+        Serial.println("°");
+      } else {
+        Serial.print("Unauthorized Tag with UID:");
+        for (int i = 0; i < rfid.uid.size; i++) {
+          Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+          Serial.print(rfid.uid.uidByte[i], HEX);
+        }
+        Serial.println();
+      }
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
+}
